39c76d8564dda9c47a20478866f90a44c2142d43
DevUrielMC <urimarcervantes.95@gmail.com>
2019-12-10 13:24:57 -0600

    Añadiendo la interfaz DAO Y creando conecxion a la base de datos

--- src/com/exercise45webservicesrest/services/ConnectionFactory.java
+++ src/com/exercise45webservicesrest/services/ConnectionFactory.java
@@ -1,14 +1,63 @@
 package com.exercise45webservicesrest.services;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class ConnectionFactory {
 
-	//Se implementa el singleton
+	//declaracion de objetos
+	static Properties props= new Properties(); 		//objeto properties
+	static InputStream in= null;	//obtenemos el dao properties
 	
+	//declaro variables
+	static String driver="";
+	static String urlServer="";
+	static String username="";
+	static String password="";
 	
 	//paso2 crear una instancia de la clase
+	
 	private static ConnectionFactory connection=null;
+	private static Connection conn=null;
+	
+	
+	public static Connection getConnection() {
+		
+		in= connection.getClass().getClassLoader().getResourceAsStream("dao.properties");	
+		
+		try {
+			props.load(in);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		if(props!=null) 
+		{
+			driver= props.getProperty("driver");
+			urlServer= props.getProperty("urlServer");
+			username= props.getProperty("username");
+			password= props.getProperty("password");
+			
+		}
+		
+		try {
+			Class.forName(driver);
+			conn= DriverManager.getConnection(urlServer, username, password);
+			
+			
+		}catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 	
 	//paso1 el metodo constructor privado
+	//Se implementa el singleton
 	private void ConnectionFactory()
 	{
 	}
