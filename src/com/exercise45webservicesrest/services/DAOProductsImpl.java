b67c3eb58c2a8682c7b26a71a8a28ebba069aefc
DevUrielMC <urimarcervantes.95@gmail.com>
2019-12-12 16:59:23 -0600

    Se añaden nuevas funcionalidades al CRUD

--- src/com/exercise45webservicesrest/services/DAOProductsImpl.java
+++ src/com/exercise45webservicesrest/services/DAOProductsImpl.java
@@ -10,8 +10,10 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 import com.exercise45webservicesrest.model.Product;
@@ -86,15 +88,89 @@ public class DAOProductsImpl implements DAOProducts {
 	}
 
 	@Override
-	public boolean DeleteProduct(int idProduct) {
-		// TODO Auto-generated method stub
-		return false;
+	@DELETE
+	@Path("/{idProduct}")
+	public boolean DeleteProduct(@PathParam("idProduct")int idProduct) {
+		conn = getConnection();
+		
+		try {
+			props.load(in);
+			if(props!=null)
+			{
+				SQLSentence=props.getProperty("SQLDeleteProduct");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try
+		{
+			pstmnt=conn.prepareStatement(SQLSentence);
+			pstmnt.setInt(1, idProduct);
+			
+			rowsAffected= pstmnt.executeUpdate();
+			System.out.println("Products DELETED TO DATABASE");
+			
+		}catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try {
+				pstmnt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return (rowsAffected>0)?true:false;
+		}
 	}
 
 	@Override
 	public Product ReadProduct(int idProduct) {
-		// TODO Auto-generated method stub
-		return null;
+		conn = getConnection();
+		Product myProduct = new Product();
+		
+		try {
+			props.load(in);
+			if(props!=null)
+			{
+				SQLSentence=props.getProperty("SQLReadProduct");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try
+		{
+			pstmnt = conn.prepareStatement(SQLSentence);
+			pstmnt.setInt(1, idProduct);
+			
+			rs= pstmnt.executeQuery();
+			//System.out.println("Products DELETED TO DATABASE");
+			rs.next();
+			myProduct.setIdProduct(rs.getInt(1));
+			myProduct.setNameProduct(rs.getString(2));
+			myProduct.setPriceProduct(rs.getDouble(3));
+			
+		}catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try {
+				rs.close();
+				pstmnt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return myProduct;
+		}
 	}
 
 	@Override
